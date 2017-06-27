70a1b18522d95e8e7421da3cbddc6d8bff6bf65e
Ben Williams and Sara Schultz <sara@>
2017-06-27 09:45:44 -0700

    add seeded data into database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+post_list = [
+  [ 'Scary', 'https://www.nytimes.com/2017/06/26/us/politics/senate-health-care-bill-republican.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=a-lede-package-region&region=top-news&WT.nav=top-news', '06-27-17', 0 ],
+  [ 'Spooky', 'http://www.skeleton.com/', '06-27-17', 0 ],
+  [ 'Salty', 'https://www.nytimes.com/2017/06/26/dining/burgerfi-vegan-beyond-burger.html?ref=dining', '06-27-17', 0 ],
+  [ 'Sweaty', 'https://www.nytimes.com/2017/06/25/us/extreme-heat-scorches-southern-arizona.html', '06-27-17', 0 ],
+]
+
+post_list.each do | title, url, created_at, score |
+  Post.create( title: title, url: url, created_at: created_at, score: score )
+end
