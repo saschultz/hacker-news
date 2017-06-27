be372f11e83f32c224109cdda426fae304933343
Ben Williams and Sara Schultz <sara@>
2017-06-27 13:53:31 -0700

    add scope for todays articles

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
+  scope :today, -> { where("created_at >=?", Time.now.beginning_of_day) }
+
   has_many :comments
-  
+
   validates(:title, :url, :presence => true)
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
+Post.destroy_all
+
 post_list = [
-  [ 'Scary', 'https://www.nytimes.com/2017/06/26/us/politics/senate-health-care-bill-republican.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=a-lede-package-region&region=top-news&WT.nav=top-news', '06-27-17', 0 ],
-  [ 'Spooky', 'http://www.skeleton.com/', '06-27-17', 0 ],
-  [ 'Salty', 'https://www.nytimes.com/2017/06/26/dining/burgerfi-vegan-beyond-burger.html?ref=dining', '06-27-17', 0 ],
-  [ 'Sweaty', 'https://www.nytimes.com/2017/06/25/us/extreme-heat-scorches-southern-arizona.html', '06-27-17', 0 ],
+  [ 'Scary', 'https://www.nytimes.com/2017/06/26/us/politics/senate-health-care-bill-republican.html?hp&action=click&pgtype=Homepage&clickSource=story-heading&module=a-lede-package-region&region=top-news&WT.nav=top-news', '2017-27-06', 0 ],
+  [ 'Spooky', 'http://www.skeleton.com/', '2017-27-06', 0 ],
+  [ 'Salty', 'https://www.nytimes.com/2017/06/26/dining/burgerfi-vegan-beyond-burger.html?ref=dining', '2017-27-06', 0 ],
+  [ 'Sweaty', 'https://www.nytimes.com/2017/06/25/us/extreme-heat-scorches-southern-arizona.html', '2017-27-06', 0 ],
+  [ 'Stingy', 'https://www.oldstuff.com', '2017-04-04', 0 ]
 ]
 
 post_list.each do | title, url, created_at, score |
